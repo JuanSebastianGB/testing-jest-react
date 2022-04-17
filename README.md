b25c8fbeef0780757ab2c600171a75b691fc378d
JuanSebastianGB <3393@holbertonschool.com>
2022-04-17 14:16:41 -0500

    UPL: Uploading project to use documentation and TDD wuth jest on React

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# testing-jest-react
+# Aplication with tests and documentation
+
+### Summary 📖
+
+This is a basic react project to implement jest tests following [TDD](https://learntdd.in/react/) and a basic documentation using jsdoc.
+
+Everything  following a minimalistic approach with one single component
+
+### Running tests (test library is integrated by default in react)
+```sh
+npm run test
+```
+### Building documentation using jsdoc
+```sh
+npm run docs
+```
+### Deploy
+
+```sh
+npm run build
+```
+### Demo:
+- [Link]()
---
+++ src/Counter/Counter.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+/**
+ * Counter component
+ * @param {object} props - Porperties
+ * @param {number} props.initialNumber -Initial Value State
+ *
+ * */
+export const Counter = ({ initialNumber }) => {
+    const [counterValue, setCounterValue] = useState(initialNumber);
+    const [inputValue, setInputValue] = useState(1);
+    const addToCounter = () => {
+        setCounterValue(counterValue + inputValue);
+    };
+    const subtractToCounter = () => {
+        setCounterValue(counterValue - inputValue);
+    }
+
+    return (
+        <>
+            <h3 data-testid='header'>Amazing Counter</h3>
+            <h2
+                data-testid='counter'
+                className={`${counterValue >= 100 ? 'green' : ''}${counterValue <= -100 ? 'red' : ''}`}
+            >
+                {counterValue}
+            </h2>
+            <button
+                data-testid='minus'
+                onClick={subtractToCounter}
+            >-</button>
+            <input
+                className='text-center'
+                data-testid='input'
+                type="text"
+                value={inputValue}
+                onChange={e => setInputValue(parseInt(e.target.value))}
+            />
+            <button
+                data-testid='plus'
+                onClick={addToCounter}
+            >+</button>
+        </>
+
+    )
+}
